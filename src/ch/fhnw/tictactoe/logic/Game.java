e5b2ace4eeb50e7610c3eaa13f5453ec3d7590fd
Fabrizio Parrillo <fabriparrilllo@hotmail.com>
2015-12-16 13:23:17 +0100

    heuristic angepasst

--- src/ch/fhnw/tictactoe/logic/Game.java
+++ src/ch/fhnw/tictactoe/logic/Game.java
@@ -19,15 +19,10 @@ public class Game {
 
     public int negmax(GameBoardModel gb, int depth, int player){
         List<Integer> moves = gb.getMoves();
-        if(heuristicValue(gb.getBoard(),player) == 10){
-            double fac = depth / 9.0 * 100;
 
-            return (int) (player * heuristicValue(gb.getBoard(),player)  *fac);
-        }
         if(depth == 0 || moves.isEmpty()){
-            double fac = depth / 9.0 * 100;
 
-            return (int) (player * heuristicValue(gb.getBoard(),player)  *fac);
+            return heuristicValue(gb.getBoard(), player);
         }
 
         int bestValue = Integer.MIN_VALUE;
@@ -43,23 +38,25 @@ public class Game {
     }
 
 
-    public int heuristicValue(int[] gb, int player){
+    public int heuristicValue(int[] gb, int p) {
         //Sieg
+        for (int player : new int[]{-1, 1}) {
 
 
+            if (gb[0] == player && gb[3] == player && gb[6] == player || // row 0
+                    gb[1] == player && gb[4] == player && gb[7] == player || // row 1
+                    gb[2] == player && gb[5] == player && gb[8] == player || // row 2
 
-        if( gb[0] == player && gb[3] == player && gb[6] == player || // row 0
-            gb[1] == player && gb[4] == player && gb[7] == player || // row 1
-            gb[2] == player && gb[5] == player && gb[8] == player || // row 2
+                    gb[0] == player && gb[1] == player && gb[2] == player || // column 0
+                    gb[3] == player && gb[4] == player && gb[5] == player || // column 1
+                    gb[6] == player && gb[7] == player && gb[8] == player || // column 2
 
-            gb[0] == player && gb[1] == player && gb[2] == player || // column 0
-            gb[3] == player && gb[4] == player && gb[5] == player || // column 1
-            gb[6] == player && gb[7] == player && gb[8] == player || // column 2
+                    gb[0] == player && gb[4] == player && gb[8] == player || // diagonal NW - SE
+                    gb[2] == player && gb[4] == player && gb[6] == player)  // diagonal NE - SW
+            {
+                return (p == -1) ? 1 : -1;
+            }
 
-            gb[0] == player && gb[4] == player && gb[8] == player || // diagonal NW - SE
-            gb[2] == player && gb[4] == player && gb[6] == player )  // diagonal NE - SW
-        {
-           return 10;
         }
         return 0;
     }
